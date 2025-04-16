b8c21466b6c4ed6c1f0c4fd922b082d6af3b6cea
devyani1512 <148792031+devyani1512@users.noreply.github.com>
2025-04-16 17:17:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ const path = require('path');
 app.use(express.json({ limit: "50mb" })); // Adjust size as needed
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(bodyParser.json());
 app.get('/', (req, res) => {
   res.render("index");
